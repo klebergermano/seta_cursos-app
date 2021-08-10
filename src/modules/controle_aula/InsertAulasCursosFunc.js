c80ddeff0d7ee5eecdc4c215e143301aeb53ce08
Kleber Germano <kleber_germano@outlook.com>
2021-08-10 14:39:07 -0300

    Edit aulas - WIP

--- src/modules/controle_aula/InsertAulasCursosFunc.js
+++ src/modules/controle_aula/InsertAulasCursosFunc.js
@@ -165,24 +165,24 @@ function createHTMLAula(aulaDados, n_aula, n_bimestre) {
      </div>
      <p>
      <span class='aula_numero'>${n_aula}</span> - 
-     <span class='aula_tema'>Tema: ${aulaDados.tema}</span>
+     <span class='aula_tema'>Tema: <span class='aula_tema_info'>${aulaDados.tema}</span></span>
      </p>
      <div class='aula_data'>
       <p>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
             <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
             <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-          </svg> ${aulaDados.data}  
+          </svg> <span class='aula_data_info'>${aulaDados.data}</span>  
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
             <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z"/>
             <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z"/>
             <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
-          </svg> ${aulaDados.horario}
+          </svg> <span class='aula_horario_info'> ${aulaDados.horario}</span>
        </p>
      </div>
       <div class='aula_detalhes'>
           <p>
-            ${aulaDados.detalhes}
+          <span class='aula_detalhes_info'> ${aulaDados.detalhes}</span>
           </p>
       </div>
      <span class='btn_deletar_aula'>
--- src/modules/controle_aula/formEditAulas.js
+++ src/modules/controle_aula/formEditAulas.js
@@ -1,16 +1,93 @@
-
+import * as formAddAulas from "./formAddAulas.js";
 
 import * as commonFunc from "../common/commonFunctions.js";
 //TODO:-----------------------------EDIT Aulas------------------------------------
 export function showEditAula(e) {
-    let addForm = document.querySelector("#form_add_aula");
-    let savePreviousHTMLForm = addForm.innerHTML;
-    addForm.classList.add("edit_form");
+    let formAddAula = document.querySelector("#form_add_aula");
+
+    //Insere as informações da aula em "#form_add_aula".
+    insertInfoInputsFormAddAula(e, formAddAula);
+
+    //Adiciona a classe edit_form no formulário.
+    formAddAula.classList.add("edit_form");
+    
+    //chama todas as funções de edição no formulário "#form_add_aula".
+    setEditAulaInfoInFormAddAula(formAddAula);
+  }
+
+  function setEditAulaInfoInFormAddAula(formAddAula){
+    //Mostra o formulário "#form_add_aula" e o bloqueio de tela "#block_screen".
+    displayFormEditAula()
+
+    //adiciona o event listener no botão de fechar o formulário.
+    addEventListenerCloseForm(formAddAula)
+
+   //disable de selects in #form_add_aula
+    disableSelectsInFormAddAula(formAddAula)
+  }
+
+  function insertInfoInputsFormAddAula(e, formAddAula){
+    let aulaInfo = getInfoInputsFormAddAula(e);
+    console.log('AulaInfo: ', aulaInfo);
+
+    formAddAula.querySelector("#tema").value = aulaInfo.tema;
+    formAddAula.querySelector("#horario").value = aulaInfo.horario;
+    formAddAula.querySelector("#data").value = aulaInfo.data;
+    formAddAula.querySelector("#detalhes").value = aulaInfo.detalhes;
+
+  }
+
+  function getInfoInputsFormAddAula(e){
+   let aula = e.target.closest('.aulas');
+   let aulaInfo = {}
+   aulaInfo.tema = aula.querySelectorAll('.aula_tema_info')[0].textContent;
+   aulaInfo.data = aula.querySelectorAll('.aula_data_info')[0].textContent;
+   aulaInfo.horario = aula.querySelectorAll('.aula_horario_info')[0].textContent;
+   aulaInfo.detalhes = aula.querySelectorAll('.aula_detalhes_info')[0].textContent;
+ 
+   return aulaInfo;
+
+  }
+
+
+
+  function addEventListenerCloseForm(formAddAula){
+    let btn_close = formAddAula.querySelectorAll(".close_form")[0];
+    btn_close.classList.add('close_form_edit');
+    btn_close.addEventListener('click', (e)=>{
+    restorePreviousStateFormAddAula(formAddAula);
+    });
+  }
+
+
+
+  //Restaura o estado anterior do formulário "#form_add_aula";
+  function restorePreviousStateFormAddAula(formAddAula){
+    removeClassEditForm(formAddAula);
+    enableSelectsInFormAddAula(formAddAula);
+  }
+
+  function displayFormEditAula(){
     commonFunc.changeCSSDisplay("#form_add_aula", "block");
     commonFunc.changeCSSDisplay("#block_screen", "block");
-    let select = addForm.querySelectorAll("select");
+  }
+
+  function disableSelectsInFormAddAula(formAddAula){
+    let select = formAddAula.querySelectorAll("select");
     select.forEach((item) => {
       item.setAttribute("disabled", true);
     });
   }
-  
+
+ //Reabilita os selectes do formulário "#form_add_aula"
+  function enableSelectsInFormAddAula(formAddAula){
+    formAddAula.querySelectorAll("select")
+    .forEach((item) => {
+      item.removeAttribute("disabled");
+    });
+  }
+  //Remove a classe ".edit_form" do formulário "#form_add_aula"
+  function removeClassEditForm(addForm){
+    addForm.classList.remove("edit_form");
+  }
+
