f0f873c1eaf9d24031b8a60c8a1ff9f4aa4c7010
Kleber Germano <kleber_germano@outlook.com>
2021-10-28 10:26:54 -0300

    Validação do input id contrato

--- src/components/geradorContrato/js/geradorIdContrato.js
+++ src/components/geradorContrato/js/geradorIdContrato.js
@@ -3,42 +3,98 @@ import { firebaseApp } from "../../dbConfig/firebaseApp.js";
 const { getFirestore, setDoc, getDocs, doc, collection } = require("firebase/firestore")
 const db = getFirestore(firebaseApp);
 
-export function toggleIdAutomaticoContrato() {
+export function eventsIdContrato() {
+    let id_contrato = document.querySelector("#id_contrato");
+
+    document.querySelector('#checkbox_id_automatico_contrato')
+        .addEventListener('click', toggleIdAutomaticoContrato);
+    insertOptionslistIdContrato();
+    //e Aplica a letra C no valor.
+    id_contrato.addEventListener('input', () => {
+        let numbers = (id_contrato.value).replace(/\D/g, '');
+        id_contrato.value = 'C' + numbers;
+    })
+    //Garante que o id tera 5 digitos aplicando padStart.
+    id_contrato.addEventListener('change', () => {
+        maskValueIdContrato()
+        validaInputIdContrato()
+    });
+
+}
+
+function toggleIdAutomaticoContrato() {
     let checkbox_id_automatico = document.querySelector("#checkbox_id_automatico_contrato");
-    let labelCheckbox =  checkbox_id_automatico.closest('label');
-    if(checkbox_id_automatico.checked){
-        console.log('on');
+    let labelCheckbox = checkbox_id_automatico.closest('label');
+    if (checkbox_id_automatico.checked) {
         labelCheckbox.classList.add('active');
         labelCheckbox.querySelector('span').innerHTML = "ID Automático Gerado &#10003;";
         setIdAutomaticoContrato();
-    }else{
-        console.log('off');
+    } else {
         labelCheckbox.classList.remove('active');
-        labelCheckbox.querySelector('span').innerHTML = "Gerar ID Automática";
-
+        labelCheckbox.querySelector('span').innerHTML = "Gerar ID Automático";
         removeIdAutomaticoContrato()
     }
-    
 }
 
-function setIdAutomaticoContrato(){
+function validaInputIdContrato() {
+    let idInputValue = document.querySelector("#id_contrato").value;
+    getIdContratos()
+        .then((idContratos) => {
+            let valida = true; 
+          idContratos.forEach((idContrato) => {
+                if (idContrato === idInputValue || idInputValue === "C000") {
+                   valida = false;
+                }
+            })
+  
+            if(!valida) invalidateInputIdContrato();
+            else validateInputIdContrato();
+
+        });
+}
+
+function invalidateInputIdContrato(){
+    let idInput = document.querySelector("#id_contrato");
+    idInput.setCustomValidity("ID em uso ou inválida");
+    idInput.classList.add('input_invalido');
+} 
+function validateInputIdContrato(){
+    let idInput = document.querySelector("#id_contrato");
+    idInput.classList.remove('input_invalido');
+
+    idInput.setCustomValidity("");
+} 
+function insertOptionslistIdContrato() {
+    let listIdContrato = document.querySelector("#list_id_contrato");
+    getIdContratos()
+        .then((idContratos) => {
+            let options = '';
+            idContratos.forEach((item) => {
+                options += `<option>${item}</option>`;
+            });
+            listIdContrato.innerHTML = options;
+        });
+
+}
+
+function setIdAutomaticoContrato() {
     let id_contrato = document.querySelector('#id_contrato');
-    let contratos_id = getContratosId();
+    let contratos_id = getIdContratos();
     id_contrato.setAttribute('readonly', true);
     contratos_id.then((res) => {
         let newIdContrato = createNewIdContrato(res)
         insertNewIdContrato(newIdContrato)
-    }).then(()=>{
+    }).then(() => {
         maskValueIdContrato()
     })
 }
 
-function removeIdAutomaticoContrato(){
+function removeIdAutomaticoContrato() {
     let id_contrato = document.querySelector('#id_contrato');
     id_contrato.removeAttribute('readonly');
-    id_contrato.value=''
+    id_contrato.value = ''
 }
-function insertNewIdContrato(newIdContrato){
+function insertNewIdContrato(newIdContrato) {
     document.querySelector('#id_contrato').value = newIdContrato;
 
 }
@@ -52,43 +108,27 @@ function createNewIdContrato(contratos_id) {
     var maxId = id_numbers.reduce(function (a, b) {
         return Math.max(a, b);
     });
-    return  maxId + 1;
+    return maxId + 1;
 }
 
-//Aplica mascara de formatação no input id_contrato 
-export function maskInputIdContrato(){
-    let id_contrato = document.querySelector("#id_contrato");
-    //Remove qualquer qualquer letra que não seja um número
-    //e Aplica a letra C no valor.
-    id_contrato.addEventListener('input', () => {
-        let numbers = (id_contrato.value).replace(/\D/g, '');
-        id_contrato.value = 'C' + numbers;
-
-    })
-    //Garante que o id tera 5 digitos aplicando padStart.
-    id_contrato.addEventListener('change', () => {
-        maskValueIdContrato()
-    });
-}
-
-function maskValueIdContrato(){
+function maskValueIdContrato() {
     let id_contrato = document.querySelector("#id_contrato");
     let numbers = getNumbersFromString(id_contrato.value);
-    let id =  formatToIdContrato(numbers)
+    let id = formatToIdContrato(numbers)
     insertNewIdContrato(id);
 }
 
-function formatToIdContrato(numbers){
+function formatToIdContrato(numbers) {
     let numbers_pad = numbers.padStart('4', 0);
-    return 'C'+numbers_pad;
+    return 'C' + numbers_pad;
 
 }
-function getNumbersFromString(string){
+function getNumbersFromString(string) {
     let numbers = (string).replace(/\D/g, '');
     return numbers
-            }
+}
 
-function getContratosId() {
+function getIdContratos() {
     let contratos_id = getDocs(collection(db, 'contratos'))
         .then((contratos) => {
             let ids = [];
