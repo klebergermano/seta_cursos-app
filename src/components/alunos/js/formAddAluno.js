52d8bd11413e2e40347c440105a188dc81f93b4f
Kleber Germano <kleber_germano@outlook.com>
2021-10-29 19:11:59 -0300

    aluno page - WIP

--- src/components/alunos/js/formAddAluno.js
+++ src/components/alunos/js/formAddAluno.js
@@ -5,7 +5,7 @@ import * as  dbAlunoHistFunc from "../../js_common/dbAlunoHistoricoFunc.js";
 import * as  alunoRA from "../../alunos/js/alunoRA.js";
 //Firebase
 import {firebaseApp} from "../../dbConfig/firebaseApp.js";
-const {getFirestore, setDoc,  doc, collection, getDocs} = require("firebase/firestore") 
+const {getFirestore, setDoc,  doc, collection, getDocs, getDoc} = require("firebase/firestore") 
 const db = getFirestore(firebaseApp);
 //-------------------------------------------------------------------------
 
@@ -20,9 +20,14 @@ function eventsFormAddAluno(){
   alunoRA.eventsAlunoRA();
 
   form.addEventListener("submit", (e) => {
+    e.preventDefault();
       submitFormAddAluno(e);
   });
   insertSelectOptionsContratos()
+
+  form.querySelector("#select_contrato").addEventListener('change', (e)=>{
+    insertInfoContrato(e)
+  })
 }
 async function insertSelectOptionsContratos(){
   const selectContrato = document.querySelector("#form_add_aluno").querySelector("#select_contrato");
@@ -33,23 +38,41 @@ async function insertSelectOptionsContratos(){
           optionsSelect += `<option value='${contrato.id}'>${contrato.id} - ${contrato.data().resp_info.nome}</option>`; 
         });
         selectContrato.innerHTML = optionsSelect;
+  }
+
+
+  function insertInfoContrato(e){
+    let IDContrato = e.target.value;
+  
+    const formAddAluno =  document.querySelector("#form_add_aluno");
+    const alunoNomeInput = formAddAluno.querySelector("#aluno_nome");
+    const respNomeInput = formAddAluno.querySelector("#resp_nome");
+    const cursoNomeInput = formAddAluno.querySelector("#curso_nome");
+    getContratoInfo(IDContrato).then((res)=>{
+      alunoNomeInput.value = res.data().aluno_info.nome;
+      respNomeInput.value = res.data().resp_info.nome;
+      cursoNomeInput.value = res.data().curso_info.nome;
+    })
   
   }
+function getContratoInfo(IDContrato){
+  
+  let contratoInfo = getDoc(doc(db, 'contratos', IDContrato));
+  return contratoInfo
 
-  function insertInfoContrato(){
-    
+}
 
-  }
-function getContratos(){
+  function getContratos(){
   const contratos = getDocs(collection(db, 'contratos'))
  return contratos;
 }
 
   //Salva o aluno no banco de dados.
   async function submitFormAddAluno(e) {
-    let form = e.target;
-    let RA = (form.add_aluno_ra.value).toUpperCase()
     e.preventDefault();
+    let form = e.target;
+    let RA = (form.aluno_ra.value).toUpperCase()
+   
      setDoc(doc(db, "alunato", RA, "cursos", form.curso_nome.value),
     { curso: form.curso_nome.value,
       bimestres: {}
@@ -58,7 +81,7 @@ function getContratos(){
      { 
        aluno: {
         ra: RA, 
-        nome: form.nome.value, 
+        nome: form.aluno_nome.value, 
         rg: "",
         email: "",
         end: "",
@@ -69,9 +92,19 @@ function getContratos(){
         cadastrado: "0000-00-00",
         obs:"",
        },
-       
-    
-    }, 
+       responsavel:{
+        nome: form.resp_nome.value, 
+        rg: "",
+        email: "",
+        end: "",
+        bairro: "",
+        cep: "",
+        data_nasc: "",
+        genero: "",
+        cadastrado: "0000-00-00",
+        obs:"",
+       }
+    },
      { merge: true}
      ); 
 
