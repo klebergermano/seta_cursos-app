6a974c54f2cb6e948f5ff0f7abce1bbdfaf822f1
Kleber Germano <kleber_germano@outlook.com>
2021-11-01 15:47:18 -0300

    contrato_page - WIP

--- src/components/contratos/js/index.js
+++ src/components/contratos/js/index.js
@@ -1,121 +1,13 @@
 
-//-----------------------------------------------------------------------
-//Components
-import inputComboCheckbox from "./inputComboCheckbox.js";
-import insertComboTextarea from "./insertComboTextarea.js";
-import insertCursoInfo from "./insertCursoInfo.js";
-import insertInputDateValue from "./insertInputDateValue.js";
-import removeAttributeFromElement from "./removeAttribute.js";
-import insertInputValorTotal from "./insertInputValorTotal.js";
-import setAttribute from "./setAttribute.js";
-import * as formAddContrato from "./formAddContrato.js";
-import * as geradorIDContrato from "./geradorIDContrato.js";
-//Firebase
-import {firebaseApp} from "../../dbConfig/firebaseApp.js";
-const {getFirestore, setDoc, getDocs, doc} = require("firebase/firestore") 
-const db = getFirestore(firebaseApp);
-//Others libraries
-const VMasker = require("vanilla-masker");
-//-------------------------------------------------------------------------
-
-function setCurso() {
-let cursosSelect1 = document.querySelector("#curso_nome");
-let cursosSelect2 = document.querySelector("#combo_curso_2");
-
-  let cursos = cursosSelect1.innerHTML;
-  cursosSelect2.innerHTML = cursos;
-};
-
-//Remove e reinsere o aluno usando css transition em ".aluno_off"
-function checkboxRespAluno(e) {
-  
-  let btn_marcar_resp_aluno = document.querySelector("#btn_marcar_resp_aluno")
-  let fieldset_aluno = document.querySelector("#fieldset_aluno");
-  e.target.parentElement.classList.toggle("active");
-  if (e.target.parentElement.classList.contains("active")) {
-    btn_marcar_resp_aluno.classList.add('btn_ativo');
-    btn_marcar_resp_aluno.querySelector('span').innerHTML = "Marcado como aluno(a) &#10003";
-    fieldset_aluno.classList.add("aluno_off");
-    //Insere o valor "IDEM" no nome do aluno
-    setAttribute("#aluno_nome", 'style', "color: #fff");
-    setAttribute("#aluno_nome", 'value', "IDEM");
-    removeAttributeFromElement("#aluno_nome", 'required');
-    removeAttributeFromElement("#aluno_parentesco", 'required');
-    removeAttributeFromElement("#aluno_genero", 'required');
-  } else {
-    btn_marcar_resp_aluno.classList.remove('btn_ativo');
-    btn_marcar_resp_aluno.querySelector('span').textContent = "Marcar como aluno(a)";
-
-    fieldset_aluno.classList.remove("aluno_off");
-    //Remove o valor "IDEM" no nome do aluno
-    setAttribute("#aluno_nome", 'style', "color:#333");
-    setAttribute("#aluno_nome", 'value', "");
-    setAttribute("#aluno_nome", 'required', true);
-    setAttribute("#aluno_parentesco", 'required', true);
-    setAttribute("#aluno_genero", 'required', true);
-  }
-}
-
+import * as contratosInfoTable from "../js/contratosInfoTable.js"
+import * as submenuPage from "../js/submenuPage.js"
+import * as commonFunc from "../../js_common/commonFunctions.js";
 
 export function onload(){
 
-  setCurso() 
-
-//variáveis
-let form = document.querySelector("#form_contrato");
-let valor = document.querySelector("#curso_valor");
-let desconto = document.querySelector("#curso_desconto");
-let vencimento = document.querySelector("#curso_vencimento");
-
-
-
-//Listeners
-form.addEventListener("submit", (e)=>{
-  formAddContrato.submitFormContrato(e)
-});
-
-//input Resp Aluno
-document
-  .querySelector("#btn_marcar_resp_aluno")
-  .addEventListener("input", checkboxRespAluno);
-
-//Button Checkbox Combo
-document
-  .querySelector("#label_check_combo")
-  .addEventListener("input", inputComboCheckbox);
-
-//Listener no nome do curso
-document
-  .querySelector("#curso_nome")
-  .addEventListener("change", insertCursoInfo);
-
-document
-  .querySelector("#combo_curso_2")
-  .addEventListener("input", insertComboTextarea);
- 
-
-valor.addEventListener("input", insertInputValorTotal);
-desconto.addEventListener("input", insertInputValorTotal);
-desconto.addEventListener("change", insertComboTextarea);
-
-
-//Mascaras
-VMasker(valor).maskMoney();
-VMasker(desconto).maskMoney();
-VMasker(document.querySelector("#resp_cep")).maskPattern("99999-999");
-VMasker(document.querySelector("#resp_cpf")).maskPattern("999.999.999-99");
-VMasker(document.querySelector("#resp_rg")).maskPattern("99.999.999-S");
-VMasker(document.querySelector("#aluno_rg")).maskPattern("99.999.999-S");
-VMasker(document.querySelector("#aluno_cep")).maskPattern("99999-999");
-
-//Insere a data corrente do dia como possível data do contrato.
-insertInputDateValue(new Date(), "#curso_data_contrato");
-//Insere a data corrente do dia como possível inicio do curso.
-insertInputDateValue(new Date(), "#curso_inicio");
-//Insere a dia atual como como possível dia de vencimento do curso.
-vencimento.value = String(new Date().getDate()).padStart(2, "0");
-
-geradorIDContrato.eventsIDContrato();
+   
+  contratosInfoTable.insertContratosInfoTable()
+  commonFunc.insertElementHTML("#bg_submenu_page", "./components/contratos/submenuPage.html",  submenuPage.eventsSubmenuPage, null, true)
 
 
 }
