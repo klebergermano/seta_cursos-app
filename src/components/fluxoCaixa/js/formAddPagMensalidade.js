bedf4504123bf6f86105da34e47a7200667f144d
Kleber Germano <kleber_germano@outlook.com>
2021-11-06 19:01:58 -0300

    Parcelas - WIP

--- src/components/fluxoCaixa/js/formAddPagMensalidade.js
+++ src/components/fluxoCaixa/js/formAddPagMensalidade.js
@@ -1,10 +1,11 @@
 
 
+var $alunoInfo = {};
 
 import * as commonFunc from "../../js_common/commonFunctions.js";
 //----------------------------------------------------
 import { firebaseApp } from "../../dbConfig/firebaseApp.js"
-const { getFirestore, doc, setDoc, onSnapshot, collection, getDocs } = require("firebase/firestore")
+const { getFirestore, doc, setDoc, onSnapshot, collection, getDocs, getDoc } = require("firebase/firestore")
 const db = getFirestore(firebaseApp);
 //------------------------------------------------------
 
@@ -12,7 +13,7 @@ export async function insertSelectAlunos(){
   onSnapshot(
      collection(db, "alunato"),
      (snap) => {
-       let selectAluno = ``;
+       let selectAluno = `<option disabled selected>Selecione o Aluno</option>`;
        snap.forEach((doc) => {
          selectAluno += `<option value='${doc.id}'>${doc.id} - ${doc.data().aluno.nome}</option>`;
        });
@@ -21,31 +22,64 @@ export async function insertSelectAlunos(){
  }
 
  function setSelectCursos(){
-   let RA =  getRAfromMainSelectAluno();
-   console.log(RA);
+  $alunoInfo.RA = getRAfromMainSelectAluno();
+  
    let selectCurso = document.querySelector("#select_curso");
-   let optionsCurso = '';
-   getDocs(collection(db, 'alunato', RA, 'cursos'))
+   let optionsCurso = '<option disabled selected>Selecione o Curso</option>';
+   getDocs(collection(db, 'alunato', $alunoInfo.RA, 'cursos'))
    .then((res)=>{
+    let cursos = {};
     res.forEach((doc)=>{
+     cursos[doc.data().curso_info.nome] = doc.data().curso_info;
+     
+    $alunoInfo.cursos = cursos;
       optionsCurso += `<option value='${doc.data().curso_info.nome}'>${doc.data().curso_info.nome}</option>`;
     })
     selectCurso.innerHTML = optionsCurso;
    });
  }
  
+ function getParcelas(nomeCurso){
+   let options = ``;
+   let parcelas = $alunoInfo.cursos[nomeCurso].parcelas;
+   let i = 1; 
+     for(let parcela of Object.values(parcelas)){
+       if(parcela.status !== 'pago'){
+        options += `<option value='parcela ${i}'>Parcela ${i}</option>`;
+       }else{
+        options += `<option disabled value='parcela ${i}'>Parcela ${i}</option>`;
+
+       }
+        i++; 
+     }
+     let selectParcelas = document.querySelector("#select_parcelas");
+     selectParcelas.innerHTML = options;
+  /*
+  console.log($alunoInfo);
+   let RA = getRAfromMainSelectAluno();
+  let idCurso = getSelectCursoID()
+
+   getDoc(doc(db, 'alunato', RA, 'cursos', idCurso))
+  .then((res)=>{
+    console.log(res.data());
+
+  })
+  */
+ }
+ function getSelectCursoID(){
+let select = document.querySelector('#select_curso');
+let idCurso = select.options[select.selectedIndex].value;
+return idCurso;
+ }
 export function insertFormPagMensalidade(){
 commonFunc.insertElementHTML("#fluxo_caixa_content", "./components/fluxoCaixa/formAddPagMensalidade.html", eventsFormPagMensalidade, null, true)
 }
-function getSelectedRA(){
-  let selectAluno = document.querySelector("#main_select_aluno");
-  let RA = selectAluno.options[selectAluno.selectedIndex].value;
-  console.log(RA)
- return RA;
-}
 
 function eventsFormPagMensalidade() {
-  
+  insertSelectAlunos()
+  document.querySelector('#select_curso').addEventListener('change', (e) => {
+    getParcelas(e.target.value)
+  });
   document.querySelector('#main_select_aluno').addEventListener('change', (e) => {
   setSelectCursos()
   });
@@ -94,7 +128,6 @@ function submitFormAddPagMensalidade(e){
             }
          }
       }
-
  
     },
      { merge: true}
--- src/components/fluxoCaixa/js/index.js
+++ src/components/fluxoCaixa/js/index.js
@@ -2,11 +2,10 @@ import * as formAddPagMensalidade from "./formAddPagMensalidade.js"
 export function onload(){
     document.querySelector("#btn_form_entrada_caixa").addEventListener('click', ()=>{
         formAddPagMensalidade.insertFormPagMensalidade();
-        formAddPagMensalidade.insertSelectAlunos();
+        //formAddPagMensalidade.insertSelectAlunos();
     });
     
     //-----------------------
-    formAddPagMensalidade.insertSelectAlunos();
     formAddPagMensalidade.insertFormPagMensalidade();
 
 }
