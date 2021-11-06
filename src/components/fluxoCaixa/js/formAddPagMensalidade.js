03295980d3ff56a87f5ef64e24b021f1e80a193d
Kleber Germano <kleber_germano@outlook.com>
2021-11-06 13:58:51 -0300

    Fluxo caixa ajustes

--- src/components/fluxoCaixa/js/formAddPagMensalidade.js
+++ src/components/fluxoCaixa/js/formAddPagMensalidade.js
@@ -4,32 +4,71 @@
 import * as commonFunc from "../../js_common/commonFunctions.js";
 //----------------------------------------------------
 import { firebaseApp } from "../../dbConfig/firebaseApp.js"
-const { getFirestore, doc, setDoc } = require("firebase/firestore")
+const { getFirestore, doc, setDoc, onSnapshot, collection, getDocs } = require("firebase/firestore")
 const db = getFirestore(firebaseApp);
 //------------------------------------------------------
 
+export async function insertSelectAlunos(){
+  onSnapshot(
+     collection(db, "alunato"),
+     (snap) => {
+       let selectAluno = ``;
+       snap.forEach((doc) => {
+         selectAluno += `<option value='${doc.id}'>${doc.id} - ${doc.data().aluno.nome}</option>`;
+       });
+     document.querySelector("#main_select_aluno").innerHTML = selectAluno;
+     })
+ }
+
+ function setSelectCursos(){
+   let RA =  getRAfromMainSelectAluno();
+   console.log(RA);
+   let selectCurso = document.querySelector("#select_curso");
+   let optionsCurso = '';
+   getDocs(collection(db, 'alunato', RA, 'cursos'))
+   .then((res)=>{
+    res.forEach((doc)=>{
+      optionsCurso += `<option value='${doc.data().curso_info.nome}'>${doc.data().curso_info.nome}</option>`;
+    })
+    selectCurso.innerHTML = optionsCurso;
+   });
+ }
+ 
 export function insertFormPagMensalidade(){
 commonFunc.insertElementHTML("#fluxo_caixa_content", "./components/fluxoCaixa/formAddPagMensalidade.html", eventsFormPagMensalidade, null, true)
 }
+function getSelectedRA(){
+  let selectAluno = document.querySelector("#main_select_aluno");
+  let RA = selectAluno.options[selectAluno.selectedIndex].value;
+  console.log(RA)
+ return RA;
+}
 
 function eventsFormPagMensalidade() {
-   
+  
+  document.querySelector('#main_select_aluno').addEventListener('change', (e) => {
+  setSelectCursos()
+  });
     document.querySelector('#form_add_pag_mensalidade').addEventListener('submit', (e) => {
         e.preventDefault();
        submitFormAddPagMensalidade(e)
     });
 }
 
+function getRAfromMainSelectAluno(){
+  let select = document.querySelector("#main_select_aluno");
+  let RA = select.options[select.selectedIndex].value;
+  return RA;
+}
 
 
 function submitFormAddPagMensalidade(e){
   e.preventDefault();
   let form = document.querySelector('#form_add_pag_mensalidade');
+  let ano = '2021';
+  let mes = 'janeiro';
+  let id = '02';
 
-    let ano = '2021';
-    let mes = 'janeiro';
-    let id = '02';
- 
       setDoc(doc(db, "fluxo_caixa", ano), 
      { 
        [mes]: {
--- src/components/fluxoCaixa/js/index.js
+++ src/components/fluxoCaixa/js/index.js
@@ -2,7 +2,11 @@ import * as formAddPagMensalidade from "./formAddPagMensalidade.js"
 export function onload(){
     document.querySelector("#btn_form_entrada_caixa").addEventListener('click', ()=>{
         formAddPagMensalidade.insertFormPagMensalidade();
+        formAddPagMensalidade.insertSelectAlunos();
     });
+    
+    //-----------------------
+    formAddPagMensalidade.insertSelectAlunos();
     formAddPagMensalidade.insertFormPagMensalidade();
 
 }
