e6b90555d2c11a2a4733a47509cb1929c7c75f3f
Kleber Germano <kleber_germano@outlook.com>
2021-12-07 13:52:33 -0300

    Correção bug controle_aula

--- src/components/controleAula/js/alunoContent.js
+++ src/components/controleAula/js/alunoContent.js
@@ -33,7 +33,7 @@ function evenstContentAula(){
 function alunoSemCursoContent(){
   let content = `
   <div class='bg_curso' style='display:block; padding-top:30px;'>
-  <h3 class='title_curso_nome'>Aluno sem cursos associados</h3>
+  <h3 class='title_curso_nome'>Aluno sem curso associado</h3>
   </div>
   `
   return content
@@ -43,11 +43,14 @@ export function insertAlunoContent(RA, snapshotChange){
     let nomeCurso = snapshotChange[0]?.doc.data()?.curso_info?.nome;
     getAlunoHistCursosDB(RA)
       .then((alunoCursosDB) => {
-        if(snapshotChange.length !== 0){
+        if(snapshotChange.length !== 0 && nomeCurso){
+          console.log(nomeCurso);
           let alunoContentHTML = createAlunoContentHTML(alunoCursosDB, RA);
           document.querySelector("#aluno_content").innerHTML = alunoContentHTML;
           navCursosAluno.insertNavCursosInBGCursos(RA, nomeCurso);
         }else{
+          console.log('vazio');
+
           document.querySelector("#aluno_content").innerHTML = alunoSemCursoContent(); 
         }
         
@@ -197,6 +200,7 @@ export function resumoBimestreBD(curso){
 function createAlunoContentHTML(alunoDataFromDB, RA) {
   let alunoContentHTML = "";
   alunoDataFromDB.forEach((resCursoDB) => {
+    
     if (typeof resCursoDB.data !== "undefined") { resCursoDB = resCursoDB.data(); }
     else { resCursoDB = resCursoDB.doc.data(); }
     //let resumoBimestre = resumoBimestreBD(resCursoDB);
@@ -210,7 +214,6 @@ function createAlunoContentHTML(alunoDataFromDB, RA) {
   });
   return alunoContentHTML;
 }
-
 function cursoVazioHTML(RA, curso) {
   return `
   <div class='bg_info_delete_curso'>
@@ -218,20 +221,23 @@ function cursoVazioHTML(RA, curso) {
       Esse curso não possui aulas adicionadas.
     </p> 
       <div class='bg_btn_deletar_curso'>
-    <button data-aluno_ra='${RA}' data-delete_curso='${curso}' class='btn_deletar_curso'>Deletar Curso</button>
-
-      <button class="btn_add btn_add_aula" id="btn_add_aula" title='Adicionar Aula' type="button">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"></path>
-        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"></path>
-        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"></path>
-      </svg>
-     Adicionar Aula
-    </button>
+        <button class="btn_add btn_add_aula" id="btn_add_aula" title='Adicionar Aula' type="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"></path>
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"></path>
+            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"></path>
+          </svg>
+        Adicionar Aula
+        </button>
       </div>
       </div>
   `
+
+//<button data-aluno_ra='${RA}' data-delete_curso='${curso}' class='btn_deletar_curso'>Deletar Curso</button>
+
+
 }
+
 
 function createResumoBimestreHTML(cursoDB, bimestreKey){
   let resBimestre = resumoBimestreBD(cursoDB);
