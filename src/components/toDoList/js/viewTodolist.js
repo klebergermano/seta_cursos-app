fe5f116c77b46fcfdee86f83e0ea35615e8aabe6
Kleber Germano <kleber_germano@outlook.com>
2022-09-01 18:28:08 -0300

    feat(w.I.P): to-do list

--- src/components/toDoList/js/viewTodolist.js
+++ src/components/toDoList/js/viewTodolist.js
@@ -1,6 +1,6 @@
 //Firebase
 import { firebaseApp } from "../../dbConfig/firebaseApp.js";
-const { getFirestore, getDocs, getDoc, collection, where, deleteDoc, query, doc } = require("firebase/firestore");
+const { getFirestore, getDocs, getDoc, setDoc, collection, where, deleteDoc, query, doc } = require("firebase/firestore");
 const db = getFirestore(firebaseApp);
 const { getAuth } = require("firebase/auth");
 const auth = getAuth(firebaseApp);
@@ -9,10 +9,28 @@ const auth = getAuth(firebaseApp);
 //import insertElementHTML from "../../components/jsCommon/insertElementHTML.js";
 import dragElementAbsolute from "../../../functions/dragElementAbsolute.js";
 
+//Functions
 import $ from "../../../functions/$.js"
 import appendFetchedElHTML from "../../../functions/appendFetchedElHTML.js";
+import getFormValuesAsObj from "../../../functions/getFormValuesAsObj.js"; 
+import sortObjIntKeysIntoArr from "../../../functions/sortObjIntKeysIntoArr.js"; 
+
+
 import pipe from "../../../functions/pipe.js";
 //---------------------------------------------------------------//
+
+//=====================================================================================//
+//=====================================================================================//
+//=====================================================================================//
+
+
+
+
+
+//=====================================================================================//
+//=====================================================================================//
+//=====================================================================================//
+
 
 const viewTodolist = (() => {
   async function appendViewTodolist() {
@@ -52,7 +70,7 @@ const viewTodolist = (() => {
 
       function setValueSubmenu(li) {
           btnSelectFlag.querySelector("span").innerHTML = li.innerHTML;
-          btnSelectFlag.querySelector("#inputValueSubmenu").value = li.textContent.trim();
+          btnSelectFlag.querySelector("#prioridade").value = li.textContent.trim();
           $(`subMenuFlags`).style.display = `none`;
       }
 
@@ -90,15 +108,69 @@ const viewTodolist = (() => {
     _eventsBtnAddTodolist();
   }
 
+
+  //Form Add To-do List -----------------------------------------------------------------
+  const _submitFormAddTodoList = ()=>{
+    $('form_add_todo').addEventListener('submit', (e)=>{
+      e.preventDefault();
+      const UID = auth.currentUser.uid; 
+      console.log(auth)
+      console.log(UID)
+      const formValueObj = getFormValuesAsObj(e.target);
+      setDocMergeToFirestoreDB(UID, formValueObj)
+    })
+  }
+
+  function setDocMergeToFirestoreDB(ID, valuesObj) {
+
+    setDoc(doc(db, 'users', ID, 'content', 'to-do_list'),
+       valuesObj,
+      { merge: true }
+    )
+      .then(() => {
+        defaultEventsAfterSubmitForm("#form_add_aula", "Aula adicionada com sucesso!")
+      }).then(() => {
+       // addLogInfo('to-do_list', 'info', `tarefa adic.`);
+      })
+      .catch((error) => {
+        console.log(error);
+        //addLogInfo('do_list', 'error', `erro ao adc.`, error);
+      });
+  }
+
+
+
   const _eventsBtnAddTodolist = () => {
     $('btn_add_todo').addEventListener('click', async (e) => {
       if (!$("form_add_todo")) (await appendFetchedElHTML('./components/todoList/formAddTodo.html'))('bg_view_table_todolist')(_eventFormAddTodo);
     });
-
   }
+
   function removeForm(e) {
     (e.target.closest('form')).remove();
   }
+
+
+
+
+//***************************************************************************************************** */
+  const getLastEntryFromDB = async ()=>{
+    const obj = await _getUserTodoList(); 
+    const sortedKeys = sortObjIntKeysIntoArr(obj); 
+    const lastArrValue = sortedKeys[sortedKeys.length - 1];
+   }
+
+   const addOneTwoStringNumberWithPadStart = (stringNumber)=>{
+    let n = parseInt(stringNumber);
+    let newNumb = (n + 1).toString().padStart("2", "0");
+    return newNumb; 
+   }
+
+
+//***************************************************************************************************** */
+
+
+
   const _eventFormAddTodo = () => {
     const btnCloseForm = $('form_add_todo').querySelector('.btn_close_form')
     btnCloseForm.addEventListener('click', (e) => {
@@ -107,13 +179,19 @@ const viewTodolist = (() => {
     })
     _eventBtnSelectFlag();
     _resetSimblingInputDate($('form_add_todo').querySelector('.btnCleanInput'))
-    _setCurrentDate($('form_add_todo').querySelector('#currentDate'))
+    _setCurrentDate($('form_add_todo').querySelector('#data'))
+    _submitFormAddTodoList();
+
+    //-----------------------------------------------------------------------------
+    getLastEntryFromDB()
+
   }
+
+//--------------------------------------------------------------------------------------------------
+
   const _insertContentTableViewHTML = (userTodolist) => {
     $('view_table_todolist').querySelector('tbody').innerHTML = createContrentTableTodolistHTML(userTodolist);
   }
-
-
 
   function createContrentTableTodolistHTML(userTodolist) {
     let tbody = document.createElement('tbody');
@@ -140,14 +218,11 @@ const viewTodolist = (() => {
           </svg>
       </button>
           </td>
-     
           `
       tr.innerHTML = trContent;
       tbody.appendChild(tr);
     }
-
     return tbody.outerHTML;
-
   }
 
   const _doubleClickWindowBarEvent = () => {
